7a36f1cb118b477c7aa7c09f28fc5cbead9e1b9d
DimitarDKirov <dimkirov@gmail.com>
2016-06-22 02:22:28 +0300

    tasks

--- Topics/03. Extension-Methods-Delegates-Lambda-LINQ/homework/HWExtensionsLINQ/2.IEnumerableExtensions/ExtensionMethods.cs
+++ Topics/03. Extension-Methods-Delegates-Lambda-LINQ/homework/HWExtensionsLINQ/2.IEnumerableExtensions/ExtensionMethods.cs
@@ -4,51 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IEnumerableExtensions
+namespace _2.IEnumerableExtensions
 {
     public static class ExtensionMethods
     {
-        public static T SumCol<T>(this IEnumerable<T> collection)
-        {
-            T result = default(T);
-            foreach (var item in collection)
-            {
-                result += (dynamic)item;
-            }
-            return result;
-        }
 
-        public static T Product<T>(this IEnumerable<T> collection)
-        {
-            dynamic result = 1;
-            foreach (var item in collection)
-            {
-                result *= (dynamic)item;
-            }
-            return result;
-        }
-
-        public static T MinCol<T>(this IEnumerable<T> collection) where T : IComparable
-        {
-            T min = collection.FirstOrDefault();
-            foreach (var item in collection)
-            {
-                if (item.CompareTo(min) < 0)
-                {
-                    min = item;
-                }
-            }
-            return min;
-        }
-
-        public static T MaxCol<T>(this IEnumerable<T> collection) where T : IComparable
-        {
-            return collection.Max();
-        }
-
-        public static T AverageCol<T>(this IEnumerable<T> collection)
-        {
-            return (dynamic)collection.SumCol() / collection.Count();
-        }
     }
 }
--- Topics/03. Extension-Methods-Delegates-Lambda-LINQ/homework/HWExtensionsLINQ/2.IEnumerableExtensions/Program.cs
+++ Topics/03. Extension-Methods-Delegates-Lambda-LINQ/homework/HWExtensionsLINQ/2.IEnumerableExtensions/Program.cs
@@ -13,12 +13,6 @@ namespace IEnumerableExtensions
     {
         static void Main(string[] args)
         {
-            ICollection<int> collection = new HashSet<int>() { 23, 45, 1, 89, 2, 10, 8, 3};
-            Console.WriteLine("Sum: {0}", collection.SumCol());
-            Console.WriteLine("Product: {0}", collection.Product());
-            Console.WriteLine("Min: {0}", collection.MinCol());
-            Console.WriteLine("Max: {0}", collection.MaxCol());
-            Console.WriteLine("Average: {0}", collection.AverageCol());
         }
     }
 }
