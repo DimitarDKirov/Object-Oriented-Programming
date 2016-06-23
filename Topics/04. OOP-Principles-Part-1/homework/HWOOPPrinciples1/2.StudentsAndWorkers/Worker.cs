cbd0ba725752ae5eb9e3e7b6c69f422ae1cb62f5
DimitarDKirov <dimkirov@gmail.com>
2016-06-24 01:53:42 +0300

    task2 started

---
+++ Topics/04. OOP-Principles-Part-1/homework/HWOOPPrinciples1/2.StudentsAndWorkers/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsAndWorkers
+{
+    class Worker : Human
+    {
+        public Worker(string firstName, string lastName) : base(firstName, lastName)
+        { }
+
+        public double WeekSalary { get; set; }
+
+        public int WorkHoursPerDay { get; set; }
+
+        public double MoneyPerHour()
+        {
+            return this.WeekSalary / (5 * this.WorkHoursPerDay);
+        }
+    }
+}
