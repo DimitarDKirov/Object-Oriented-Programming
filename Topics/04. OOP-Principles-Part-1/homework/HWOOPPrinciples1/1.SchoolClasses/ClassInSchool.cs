e0d121218983905d90883d169d56d410bb52909a
DimitarDKirov <dimkirov@gmail.com>
2016-06-24 01:33:34 +0300

    task 1 ready

---
+++ Topics/04. OOP-Principles-Part-1/homework/HWOOPPrinciples1/1.SchoolClasses/ClassInSchool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolClasses
+{
+    class ClassInSchool : ICommentable
+    {
+        public List<Teacher> Teachers { get; set; }
+
+        public List<Student> Students { get; set; }
+
+        public string Identifier { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
+
---
+++ Topics/04. OOP-Principles-Part-1/homework/HWOOPPrinciples1/1.SchoolClasses/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolClasses
+{
+    class Teacher:Person
+    {
+        public List<Discipline> Disciplines { get; set; }
+    }
+}
