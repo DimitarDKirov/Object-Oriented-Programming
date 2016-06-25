a46b20a83fce01a5b6f0081956048b80cf482b05
Dimitar Kirov <Dimitar Kirov>
2016-06-26 01:24:24 +0300

    Added Homework OOP Principles - Part 2, task 1

---
+++ Topics/05. OOP-Principles-Part-2/homework/HWOOPPrinciples2/1.Shapes/Program.cs
@@ -0,0 +1,30 @@
+﻿//Problem 1. Shapes
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[]
+            {
+                new Triangle(10,20),
+                new Rectangle(10,20),
+                new Square(10)
+            };
+
+            Console.WriteLine("Shapes surface");
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine("{0} - {1}", shape.GetType().Name, shape.CalculateSurface());
+            }
+
+        }
+    }
+}
