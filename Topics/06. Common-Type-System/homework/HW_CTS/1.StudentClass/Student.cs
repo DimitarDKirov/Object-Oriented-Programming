8512421e559aef577524f6343207081ea4ff61eb
DimitarDKirov <dimkirov@hotmail.com>
2016-06-29 19:08:06 +0300

    Homework: Common Type System task 1 started

---
+++ Topics/06. Common-Type-System/homework/HW_CTS/1.StudentClass/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.StudentClass
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public string SSN { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int Course { get; set; }
+
+    }
+}
